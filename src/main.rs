a9831279e62079029c91cb25053e02a58d2bfa7c
AlexPeixoto <alex.caldas@gmail.com>
2021-03-18 20:30:23 +0000

    Add basic files for the emulator and also some commented usefull code as a reference. The code is currently in a very messy state (more like a playground to learn RUST).

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+extern crate sfml;
+
+mod keyboard;
+
+/*use sfml::{
+    audio::{Sound, SoundBuffer},
+    graphics::{
+        CircleShape, Color, Font, RectangleShape, RenderTarget, RenderWindow, Shape, Text,
+        Transformable,
+    },
+    system::{Clock, Time, Vector2f},
+    window::{ContextSettings, Event, Key, Style},
+};*/
+use sfml::window::{ContextSettings, Event, Style};
+use sfml::graphics::{RenderWindow};
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = RenderWindow::new(
+        (800, 600),
+        "RustyChip8 Emulator",
+        Style::CLOSE,
+        &ContextSettings::default(),
+    );
+
+    while window.is_open() {
+        while let Some(event) = window.poll_event() {
+            // Request closing for the window
+            if event == Event::Closed {
+                window.close();
+            }
+        }
+
+        //window.set_active(true);
+        window.display();
+    }
+
+    /*let mut window = match RenderWindow::new(VideoMode::new_init(800, 600, 32),
+                                             
+                                             Style::CLOSE,
+                                             &ContextSettings::default()) {
+        Some(window) => window,
+        None => panic!("Cannot create a new Render Window.")
+    };*/
 }
